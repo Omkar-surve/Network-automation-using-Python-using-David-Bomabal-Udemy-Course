fb7fac81ef4927474250fa4309956a7adbc3f5b0
Omkar Surve <omkarsurve1919@gmail.com>
2019-08-24 13:44:57 +0530

    able to save configurations into files
    able to save configurations into files

--- backup_network_device_config.py
+++ backup_network_device_config.py
@@ -1,5 +1,6 @@
 import getpass
 import telnetlib
+import os
 
 username = input("Enter your telnet username: "+"\n")
 password = getpass.getpass()
@@ -12,8 +13,8 @@ file = open('switches_mgmt_ip')
 for IP in file:
     print("Backup running config for switch "+IP+"\n")
     IP = IP.strip()
-    HOST = IP
-    tn = telnetlib.Telnet(HOST)
+#    HOST = IP
+    tn = telnetlib.Telnet(IP)
     tn.read_until(b"Username: ")
     tn.write(username.encode('ascii')+b"\n")
     tn.read_until(b"Password: ")
@@ -25,4 +26,11 @@ for IP in file:
     tn.write(b"terminal length 0 \n")
     tn.write(b"sh run \n")
     tn.write(b"exit \n")
-    print(tn.read_all().decode('ascii')+"\n")
+
+    running_config = tn.read_all()
+    save_running_config = open('Switch '+IP,'w')
+    save_running_config.write(running_config.decode('ascii'))
+    save_running_config.close()
+
+    print("Running config for switch "+IP+" is saved in file "+"Switch "+IP+"\n")
+#    print(tn.read_all().decode('ascii')+"\n")
