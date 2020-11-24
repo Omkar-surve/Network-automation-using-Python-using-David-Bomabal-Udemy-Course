7d40bbc85aa7466780189432e8362f6714e3a73b
root <root@eve-ng.example.com>
2020-11-24 17:50:26 +0200

    EVE lab file

--- new practise/labs/backup_labs.sh
+++ new practise/labs/backup_labs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-#SRC_DIR="/opt/unetlab/labs/"
-
-#DST_DIR="/root/git_doc/Network-automation-using-Python-using-David-Bomabal-Udemy-Course/new practise/labs/"
+#Copying source folder to backup labs folder
 cp -a /opt/unetlab/labs/ /root/git_doc/Network-automation-using-Python-using-David-Bomabal-Udemy-Course/new\ practise/labs/
 
+#changing path to git init folder
 cd /git_doc/
+
+#staging git folders  for commit
 git add .
 
+#checking staged files
 git status
 
+#commiting files to git
 git commit -m "EVE lab file"
 
+#adding remote path to git
 git remote  add origin https://github.com/Omkar-surve/Network-automation-using-Python-using-David-Bomabal-Udemy-Course.git
 
+#pushing files to git
 git push -u origin master
