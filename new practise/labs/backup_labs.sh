6741abd2e5529aa74b451bbd87cd13837e15511c
root <root@eve-ng.example.com>
2020-11-24 16:33:08 +0200

    EVE lab file

--- new practise/labs/backup_labs.sh
+++ new practise/labs/backup_labs.sh
@@ -4,7 +4,7 @@ SRC_DIR="/opt/unetlab/labs/"
 
 DST_DIR="/git_doc/Network-automation-using-Python-using-David-Bomabal-Udemy-Course/new practise/labs/"
 
-cp $SRC_DIR/ $DST_DIR
+cp -r $SRC_DIR/ $DST_DIR
 
 cd /git_doc/
 
